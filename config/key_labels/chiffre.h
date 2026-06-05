dc9acfcd41c3d2b0ba52fbf063e0d4500708a6d4
sy stroud <stroudafk@gmail.com>
2026-06-04 19:45:54 -0500

    edit chiff header

--- config/key_labels/chiffre.h
+++ config/key_labels/chiffre.h
@@ -30,7 +30,7 @@
   │  0   1   2   3   4 │  5   6   7   8   9 │ │ LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 │
   │ 10  11  12  13  14 │ 15  16  17  18  19 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
   │ 20  21  22  23  24 │ 25  26  27  28  29 │ │ LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 │
-  ╰───────╮ 30  31  32 │ 33  34  35 ╭───────╯ ╰───────╮ LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───────╯
+  ╰───────╮     30  31 │ 32  33     ╭───────╯ ╰───────╮     LH1 LH0 │ RH0 RH1     ╭───────╯
           ╰────────────┴────────────╯                 ╰─────────────┴─────────────╯             */
 
 #pragma once
@@ -71,30 +71,39 @@
 #define RB3 28
 #define RB4 29
 
-#define LH0 32  // left thumb keys
-#define LH1 31
-#define LH2 30
+#define LH0 31  // left thumb keys
+#define LH1 30
 
-#define RH0 33  // right thumb keys
-#define RH1 34
-#define RH2 35
+#define RH0 32  // right thumb keys
+#define RH1 33
 
 #define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // left-hand keys
 #define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // right-hand keys
-#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                      // thumb keys
+#define THUMBS LH1 LH0 RH0 RH1 // thumb keys
 
-#define LAYER_FROM_SUPERSET( \
-    k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, \
-    k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, \
-    k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, \
-          k36, k37, k38, ENCODER, k39, k40, k41 \
+#define LAYER_FROM36( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
+              k30, k31, k32, k33, k34, k35            \
 ) \
-    k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, \
-    k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, \
-    k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, \
-                   k37, k38, ENCODER, k39, k40 \
+    k00  k01  k02  k03  k04  k05  k06  k07  k08  k09  \
+    k10  k11  k12  k13  k14  k15  k16  k17  k18  k19  \
+    k20  k21  k22  k23  k24  k25  k26  k27  k28  k29  \
+                  k31  k32 ENCODER k33  k34
 
-// #define LAYER_FROM_SUPERSET( \
+// #define LAYER_FROM36( \
+//     k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, \
+//     k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, \
+//     k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, \
+//           k36, k37, k38, ENCODER, k39, k40, k41 \
+// ) \
+//     k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, \
+//     k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, \
+//     k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, \
+//                    k37, k38, ENCODER, k39, k40 \
+
+// #define LAYER_FROM36( \
 //     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, \
 //     k12, k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, k23, \
 //     k24, k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, k35, \

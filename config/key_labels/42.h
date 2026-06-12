53d51a233a79417745d06b61fab19eabe4713fc5
sy stroud <stroudafk@gmail.com>
2026-06-12 14:35:24 -0500

    fix incorrect mapping to corne

--- config/key_labels/42.h
+++ config/key_labels/42.h
@@ -69,7 +69,7 @@
 #define CORE( \
     vtab, k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, vbspc, \
     vesc, k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, vent, \
-    vlshft, k21, k22, k23, k24, k25, k26, k27, k28, k29, vrshft, \
+    vlshft, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, vrshft, \
               k30, k31, k32, ENCODER, k33, k34, k35   \
 ) \
     vtab k00  k01  k02  k03  k04  k05  k06  k07  k08  k09 vbspc \

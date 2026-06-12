f567141eb0c1c67a3a49d6111bc63d3acb38d083
sy stroud <stroudafk@gmail.com>
2026-06-12 14:14:07 -0500

    resolve missed 6 col additions

--- config/key_labels/36.h
+++ config/key_labels/36.h
@@ -84,9 +84,9 @@
 #define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                      // thumb keys
 
 #define CORE( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
-    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
-    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
+    vtab, k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, vbspc, \
+    vesc, k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, vent, \
+    vlshft, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, vrshft, \
               k30, k31, k32, ENCODER, k33, k34, k35   \
 ) \
     k00  k01  k02  k03  k04  k05  k06  k07  k08  k09  \
